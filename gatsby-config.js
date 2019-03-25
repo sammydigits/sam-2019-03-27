038441e77c6b70cba9bf87d99581ad19bbf92697
Sam Thompson <Sam.Thompson@gmail.com>
2019-03-25 13:38:31 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `https://sam-2019-03-27.sam-thompson.info`,
+        apiBase: `api`, // optional, defaults to `jsonapi`
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
