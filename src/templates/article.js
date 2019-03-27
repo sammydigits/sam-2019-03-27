2dc232a7394a1c7354b785e765f136c2fedc376f
Sam Thompson <Sam.Thompson@gmail.com>
2019-03-26 23:45:10 -0500

    add meta description function

--- src/templates/article.js
+++ src/templates/article.js
@@ -2,11 +2,19 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Img from "gatsby-image"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
   const post = data.nodeArticle
+
+  function createMetaDescription() {
+    // strip html and reduce string length to 150 chars (recommended for SEO)
+    return post.body.value.replace(/<[^>]+>/g, "").substr(0, 150)
+  }
+
   return (
     <Layout>
+      <SEO title={post.title} description={createMetaDescription()} />
       <div className="article">
         <div className="image">
           <Img
@@ -16,21 +24,18 @@ export default ({ data }) => {
           />
         </div>
         <div className="content">
-          <h1>{post.title}</h1>
+          <h2>{post.title}</h2>
           <small>
             <em>{post.created}</em>
           </small>
-
-          <div
-            className="body"
-            dangerouslySetInnerHTML={{ __html: post.body.value }}
-          />
+          <div dangerouslySetInnerHTML={{ __html: post.body.value }} />
         </div>
       </div>
     </Layout>
   )
 }
 
+//drupal_internal__nid is stored in Context by createPage function in gatsby-node.js
 export const query = graphql`
   query($drupal_internal__nid: Int!) {
     nodeArticle(drupal_internal__nid: { eq: $drupal_internal__nid }) {
